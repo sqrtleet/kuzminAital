1949449d61b3d1be439de8dc0c4cda7578377b9b
Sergei <sqrtleet@gmail.com>
2024-02-21 00:33:46 +0900

    qq

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import TabNavigation from "./src/navigations/navigation";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { Provider } from "react-redux";
+import store from "./src/redux";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://rickandmortyapi.com/graphql/",
+  cache: new InMemoryCache(),
 });
+
+const App = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <NavigationContainer>
+          <TabNavigation />
+        </NavigationContainer>
+      </Provider>
+    </ApolloProvider>
+  );
+};
+
+export default App;
---
+++ src/screens/task1.js
@@ -0,0 +1,108 @@
+import { useState, useEffect } from "react";
+import {
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+  Button,
+  Switch,
+} from "react-native";
+
+import { useSelector } from "react-redux";
+
+const colors = ["white", "black"];
+const colorstext = ["black", "white"];
+
+const Task1 = () => {
+  const count3 = useSelector((state) => state.counter.value);
+  const [count, setCount] = useState(0);
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+  const [backgroundColorIndex, setBackgroundColorIndex] = useState(0);
+
+  const plusClick = () => {
+    setCount(count + 1);
+  };
+
+  const minusClick = () => {
+    if (count > 0) setCount(count - 1);
+  };
+
+  const themes = () => {
+    if (!isEnabled) {
+      setBackgroundColorIndex((backgroundColorIndex + 1) % colors.length);
+    } else {
+      setBackgroundColorIndex(0);
+    }
+  };
+
+  return (
+    <SafeAreaView
+      style={{
+        backgroundColor: colors[backgroundColorIndex],
+        flex: 1,
+        justifyContent: "center",
+      }}
+    >
+      <View style={{ margin: 20 }}>
+        <View style={{ alignItems: "center" }}>
+          <Text
+            style={{
+              color: colorstext[backgroundColorIndex],
+              fontSize: 20,
+              fontWeight: "800",
+            }}
+          >
+            {count}
+          </Text>
+        </View>
+        <TouchableOpacity style={styles.button} onPress={plusClick}>
+          <Text style={{ color: colors[backgroundColorIndex] }}>+</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={minusClick}>
+          <Text style={{ color: colors[backgroundColorIndex] }}>-</Text>
+        </TouchableOpacity>
+        <View style={styles.container}>
+          <Switch
+            trackColor={{ false: "#767577", true: "#45ff83" }}
+            thumbColor={isEnabled ? "#f4f3f4" : "white"}
+            ios_backgroundColor="#f2f2f7"
+            onValueChange={toggleSwitch}
+            onChange={themes}
+            value={isEnabled}
+          />
+        </View>
+        <View style={{ alignItems: "center",marginVertical: 20}}>
+          <Text
+            style={{
+              color: colorstext[backgroundColorIndex],
+              fontSize: 20,
+              fontWeight: "800",
+            }}
+          >
+            {count3}
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    margin: 10,
+  },
+
+  button: {
+    alignItems: "center",
+    backgroundColor: "#696D70",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+    margin: 5,
+  },
+});
+export default Task1;
